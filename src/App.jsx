86ebb55772e66128322c737f1185fe0289e97021
AkdemicosPruebas <akemicospruebas@gmail.com>
2024-12-27 19:55:32 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import './App.css'
 import Header from './Pages/Header'
 import Login from './Pages/Login'
 import Home from './Pages/Home';
+import Cursos from './Pages/Cursos';
+import AboutMe from './Pages/AboutMe';
+import CalculoDiferencial from './Pages/CalculoDiferencial';
+import Footer from './Pages/Footer';
+import CompEjercicios from './components/CompEjercicios';
 
 function App() {
 
@@ -12,7 +17,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<Login/>} />
+          <Route path='/cursos' element={<Cursos/>}/>
+          <Route path='/proyectos' element={<CompEjercicios/>}/>
+          <Route path='/about-me' element={<AboutMe/>}/>
+          <Route path='/cursos/CalculoDiferencial' element={<CalculoDiferencial/>}/>
         </Routes>
+      <Footer/>      
     </HashRouter>
   );    
   
